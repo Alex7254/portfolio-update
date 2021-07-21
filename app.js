c3b01c829e6502219aadc9e01ce704fee67769a7
Alejandro <alejandro199898@gmail.com>
2021-07-21 16:32:17 -0700

    get elements with class of work-experience__card

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const buttonRight = document.getElementById('arrow-right');
 const buttonLeft = document.getElementById('arrow-left');
-
-buttonRight.onclick = function () {
-  document.getElementById('projects-container').scrollLeft += 950;
-};
-buttonLeft.onclick = function () {
-  document.getElementById('projects-container').scrollLeft -= 950;
-};
+const workExperienceCard = document.querySelectorAll('.work-experience__card');
+// Navigation arrows for projects section
+buttonRight.onclick = () => document.getElementById('projects-container').scrollLeft += 950;
+buttonLeft.onclick = () => document.getElementById('projects-container').scrollLeft -= 950;
+// Work Experience Card expanding function
+console.log(workExperienceCard.length); 
