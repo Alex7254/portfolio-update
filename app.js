fb85976494a45390cd951118077c4738edd179a6
Alejandro <alejandro199898@gmail.com>
2021-11-18 11:42:51 -0800

    replace var with let

--- app.js
+++ app.js
@@ -8,28 +8,24 @@ const technicalkillsCards = document.querySelectorAll(
   ".technical-skills__card"
 );
 
-// Project Navigation Arrows
-buttonRight.onclick = () => (projects.scrollLeft += 966);
-buttonLeft.onclick = () => (projects.scrollLeft -= 966);
-
+// Functions
 // Loops through work experience cards to toggle open/closed css classes
 for (let i = 0, n = workExperienceCard.length; i < n; ++i) {
   workExperienceCard[i].addEventListener("click", function () {
     this.classList.toggle("work-experience__card_expanded");
     // Get text inside of work experience card
-    var content = this.children[1];
+    let content = this.children[1];
     // Toggle text inside of work experience card
     content.style.display === "block"
       ? (content.style.display = "none")
       : (content.style.display = "block");
   });
 }
-
-// Function to check if element is visible from https://stackoverflow.com/a/22480938
+// Check if element is visible. From https://stackoverflow.com/a/22480938
 function isScrolledIntoView(el) {
-  var rect = el.getBoundingClientRect();
-  var elemTop = rect.top;
-  var elemBottom = rect.bottom;
+  let rect = el.getBoundingClientRect();
+  let elemTop = rect.top;
+  let elemBottom = rect.bottom;
 
   // Only completely visible elements return true:
   // var isVisible = elemTop >= 0 && elemBottom <= window.innerHeight;
@@ -37,19 +33,24 @@ function isScrolledIntoView(el) {
   isVisible = elemTop < window.innerHeight && elemBottom >= 0;
   return isVisible;
 }
-document.addEventListener(
-  "scroll",
-  function () {
-    if (isScrolledIntoView(technicalkills)) {
-      technicalkillsCards.forEach((skillCard) =>
-        skillCard.classList.add("fade-in")
-      );
-    }
-  },
-  {
-    passive: true,
+// Skill card fade in for scroll event listener
+function skillCardFadeIn() {
+  if (isScrolledIntoView(technicalkills)) {
+    technicalkillsCards.forEach((skillCard) =>
+      skillCard.classList.add("fade-in")
+    );
   }
-);
+}
 
-// console.log(isScrolledIntoView(technicalkills));
-// console.log(technicalkills);
+// Event Listeners
+// Fade skill cards into view when visible
+document.addEventListener("scroll", skillCardFadeIn, {
+  passive: true,
+});
+// Fade work experience cards into view when visible
+// document.addEventListener("scroll", workExperienceFadeIn, {
+//   passive: true,
+// });
+// Project Navigation Arrows
+buttonRight.onclick = () => (projects.scrollLeft += 966);
+buttonLeft.onclick = () => (projects.scrollLeft -= 966);
