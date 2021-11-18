c6a922bc198b56723777871b9b81ef3080970fb3
Alejandro <alejandro199898@gmail.com>
2021-11-18 11:54:54 -0800

    create function to toggle expanding work experience cards

--- app.js
+++ app.js
@@ -9,24 +9,21 @@ const technicalkillsCards = document.querySelectorAll(
 );
 
 // Functions
-// Loops through work experience cards to toggle open/closed css classes
-for (let i = 0, n = workExperienceCard.length; i < n; ++i) {
-  workExperienceCard[i].addEventListener("click", function () {
-    this.classList.toggle("work-experience__card_expanded");
-    // Get text inside of work experience card
-    let content = this.children[1];
-    // Toggle text inside of work experience card
-    content.style.display === "block"
-      ? (content.style.display = "none")
-      : (content.style.display = "block");
-  });
+// Toggle expanding work experience cards
+function toggleExpand() {
+  this.classList.toggle("work-experience__card_expanded");
+  // Get text inside of work experience card
+  let content = this.children[1];
+  // Toggle text inside of work experience card
+  content.style.display === "block"
+    ? (content.style.display = "none")
+    : (content.style.display = "block");
 }
 // Check if element is visible. From https://stackoverflow.com/a/22480938
 function isScrolledIntoView(el) {
   let rect = el.getBoundingClientRect();
   let elemTop = rect.top;
   let elemBottom = rect.bottom;
-
   // Only completely visible elements return true:
   // var isVisible = elemTop >= 0 && elemBottom <= window.innerHeight;
   // Partially visible elements return true:
@@ -43,6 +40,10 @@ function skillCardFadeIn() {
 }
 
 // Event Listeners
+// Loops through work experience cards to toggle open/closed css classes
+for (let i = 0, n = workExperienceCard.length; i < n; ++i) {
+  workExperienceCard[i].addEventListener("click", toggleExpand);
+}
 // Fade skill cards into view when visible
 document.addEventListener("scroll", skillCardFadeIn, {
   passive: true,
